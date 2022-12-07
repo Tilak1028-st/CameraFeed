600f426b75c854bb614358744fd39be22b1353c3
Shakya Tilak <shakyatilak45@gmail.com>
2022-12-07 12:30:00 +0530

    Initial Commit

---
+++ CameraFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraFeed
+//
+//  Created by PCS213 on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
