d9a79c23710eacc8dc40d7130c03aa240e1fa326
Shakya Tilak <shakyatilak45@gmail.com>
2022-12-07 13:15:34 +0530

    Design Camera Screen

--- CameraFeed/ViewController.swift
+++ CameraFeed/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toggleFlashButton: UIButton!
+    @IBOutlet weak var cameraPreviewView: UIView!
+    @IBOutlet weak var toogleCameraButton: UIButton!
+    @IBOutlet weak var photoModeButton: UIButton!
+    @IBOutlet weak var videoModeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
